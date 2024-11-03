16ed498d63f725862b71b627175101ef49780d76
joy111joy <150044778+joy111joy@users.noreply.github.com>
2024-11-03 18:25:39 -0330

    Update ClientApplicationTest.java

--- src/test/java/com/keyin/ClientApplicationTest.java
+++ src/test/java/com/keyin/ClientApplicationTest.java
@@ -17,10 +17,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ClientApplicationTest {
 
     @Mock
-    private HttpClient httpClient;
+    public HttpClient httpClient;
 
     @InjectMocks
-    private ClientApplication.AirportClientApp clientApp;
+    public ClientApplication.AirportClientApp clientApp;
 
     @BeforeEach
     public void setUp() throws Exception {
